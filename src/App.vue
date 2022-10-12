f11fbd2aa640936958ae08fb0c36878ce6c8bf1b
satoshi <highfive0228@gmail.com>
2022-10-12 17:51:10 +0300

    макет лифта

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="elevator-screen"></div>
+  <div class="elevator-content">
+    <div class="elevator-container" :style="elevatorContainerStyle">
+      <template v-for="floor in floors" :key="floor">
+        <Cell
+          v-for="elevator in elevators"
+          :key="elevator"
+          :floor="floor"
+        ></Cell>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useStore } from "vuex";
+import { ref, computed } from "vue";
+import Cell from "./components/Elevator-cell.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Cell: Cell,
+  },
+  setup() {
+    const store = useStore();
+
+    const floors = ref(5);
+    const elevators = ref(2);
+
+    const floorHeight = ref("150px");
+    const elevatorWidth = ref("150px");
+
+    const elevatorContainerStyle = computed(() => {
+      let columns = "";
+      let rows = "";
+      for (let i = 0; i < elevators.value; i++) {
+        columns = columns + " " + floorHeight.value;
+      }
+      for (let i = 0; i < floors.value; i++) {
+        rows = rows + " " + elevatorWidth.value;
+      }
+      console.log(columns);
+      return `grid-template-columns:${columns}; grid-template-rows:${rows}`;
+    });
+
+    return { elevatorContainerStyle, floors, elevators };
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.elevator-container {
+  position: relative;
+  display: grid;
+  margin: auto;
+}
+
+.elevator-content {
+  margin: 0 auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  display: flex;
+  grid-template-columns: 1fr auto 1fr;
+  grid-auto-rows: auto;
+}
+
+.elevator-screen {
+  height: 200px;
 }
 </style>
---
+++ src/components/Elevator-cell.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cell">
+    <div class="elevator-button"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["floor"],
+  name: "Cell",
+  setup(props) {
+    console.log(props.floor);
+  },
+};
+</script>
+
+<style lang="scss">
+.cell {
+  position: relative;
+  border: solid black 1px;
+}
+.elevator-button {
+  height: 20px;
+  width: 20px;
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  bottom: 50%;
+  margin: auto;
+  border: solid rgb(2, 2, 121) 1px;
+  cursor: pointer;
+  &:after {
+    content: "";
+    position: absolute;
+    border-radius: 50%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 35%;
+    height: 35%;
+    background-color: blue;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    border-radius: 50%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+    height: 70%;
+    border: solid rgb(2, 2, 121) 1px;
+  }
+}
+</style>
